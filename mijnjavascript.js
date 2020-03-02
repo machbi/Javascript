0c294e7791eafe916e27dccb720a1d85f4f91523
Mark Bijl <m>
2020-03-02 11:18:17 +0100

    Hoofdstuk 1 klaar 2.1 klaar, beginnen met 2.2

--- mijnjavascript.js
+++ mijnjavascript.js
@@ -1,8 +1,24 @@
+// Opdracht 1.1
+console.log("Hello world");
+
+// Opdracht 1.2
+for(var i=0; i<10; i++){
+    console.log("i heeft nu de waarde" + i)
+}
+
 var a =10;
 if (a > 9){
     console.log("Hallo, A is groter dan 9")
 }
 
-for(var i=0; i<10; i++){
-    console.log("i heeft nu de waarde" + i)
+// Opdracht 1.3
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: "4",
+    kleur: "blauw"
 }
+
+console.log(mijnauto.kleur);
+
+document.getElementById("grotekop").innerHTML = mijnauto.kleur;
---
+++ vulmetjs.js
@@ -0,0 +1,5 @@
+// Opdracht 2.1
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "img/aap1.jpeg";
+document.getElementById("aap2").src = "img/aap2.jpeg";
