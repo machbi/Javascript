395746d378e92eb30af15fc4b1013abab5cf8ed0
Mark Bijl <m>
2020-03-04 00:56:00 +0100

    Hoofdstuk 2 klaar.

--- 002blokjes.js
+++ 002blokjes.js
@@ -4,6 +4,6 @@ document.getElementById("bl2").style.backgroundColor = "green";
 document.getElementById("bl3").style.backgroundColor = "yellow";
 document.getElementById("bl4").style.backgroundColor = "red";
 
-// marginTops
+// margin Tops
 document.getElementById("bl2").style.marginTop = "20px";
 document.getElementById("bl3").style.marginTop = "40px";
